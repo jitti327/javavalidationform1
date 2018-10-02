25448f6a51bb72dc03daa06018e98d05f7f27e39
Jitti327 <jitti327@gmail.com>
2018-10-02 18:42:04 +0530

    lidation methos

---
+++ Create a page name it has singup.php
@@ -0,0 +1,37 @@
+ Create a page name it has singup.php
+
+
+-- Add following fields in the form
+
+  -- Name
+    -- Max length 50
+  -- Email
+  -- Password
+    -- Not more then 15 character
+    -- Not less then 8 characters
+    -- One Uppercase letter
+    -- Have atleast one special character
+  -- Confirm Password
+  -- Gender
+     -- Type radio
+     -- Male, Female, Other
+  -- Qualifcation
+     -- Type dropdown
+     -- 10th, 12th, Bachelor, Master, Phd
+
+
+ Insert these records in database.
+
+
+ b) Create a page name it as listing.php
+
+
+  -- Show all the records that are save in the database in tabular form
+  -- Add the end have a column name it has 'Action'.
+  -- Each action column will have two option 'edit' & 'delete'
+
+c) Once user click on 'edit', he is taken to new page name it has
+edit.php. Here details of all previous entry will appear pre-filled.
+
+c) Once user click on 'delete', he is shown a alert box, asking him to
+confirm the action.
